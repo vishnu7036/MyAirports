e9030133fefdab01bf89fd137d0dcf3c74b8371e
vishnu <vishnu.g@mind-graph.com>
2024-10-01 10:21:58 +0530

    APK file uploaded

---
+++ test-output-thread/report.js
@@ -0,0 +1,4 @@
+$(document).ready(function() {
+CucumberHTML.timelineItems.pushArray([{"id":"b4c882c8-4ced-48a2-a8f3-63678895d889","feature":"Verifying the General Page","scenario":"Verify the About Page","start":1727688825088,"group":1,"content":"","tags":"@general,","end":1727688995838,"className":"passed"}]);
+CucumberHTML.timelineGroups.pushArray([{"id":1,"content":"Thread[#1,main,5,main]"}]);
+});
